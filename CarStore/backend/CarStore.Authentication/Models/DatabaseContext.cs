364a8e90e63ab57c2471c9c16c0d673678b82ba3
DESKTOP-R2ETOD7\TOSHIBA-HUY <chenhchihuyckc@gmail.com>
2022-04-27 06:05:52 +0700

    Commit code for CarStore.Authentication.

---
+++ CarStore/backend/CarStore.Authentication/Models/DatabaseContext.cs
@@ -0,0 +1,36 @@
+﻿using CarStore.Authentication.Commons;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarStore.Authentication.Models
+{
+    public class DatabaseContext : IdentityDbContext<ApplicationUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole()
+                {
+                    Id = "asdfghjklmnbvcxzqwertyuio01",
+                    Name = CarStoreConstants.SuperUserRole,
+                    NormalizedName = CarStoreConstants.SuperUserRole
+                },
+                new IdentityRole()
+                {
+                    Id = "asdfghjklmnbvcxzqwertyuio02",
+                    Name = CarStoreConstants.UserRole,
+                    NormalizedName = CarStoreConstants.UserRole
+                }
+            );
+        }
+
+    }
+}
