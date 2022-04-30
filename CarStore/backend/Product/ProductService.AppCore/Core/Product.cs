a767b3bb409d34ca3b2ad0f7133f1c4315614cec
Tung Nguyen Dang <tung.nguyendang@nashtechglobal.com>
2022-04-30 18:31:45 +0700

    Update Product model and seed data

--- CarStore/backend/Product/ProductService.AppCore/Core/Product.cs
+++ CarStore/backend/Product/ProductService.AppCore/Core/Product.cs
@@ -1,38 +1,74 @@
-﻿using CarStore.IntegrationEvents.Product;
+﻿using CarStore.AppContracts.Dtos;
+using CarStore.IntegrationEvents.Product;
 using N8T.Core.Domain;
 
 namespace ProductService.AppCore.Core
 {
     public class Product : EntityRootBase
     {
-        public string Name { get; private init; } = default!;
-        public bool Active { get; private init; }
-        public int Quantity { get; private init; }
-        public decimal Cost { get; private init; }
+        public string Name { get; private set; }
 
-        public static Product Create(string name, int quantity, decimal cost)
-        {
-            return Create(Guid.NewGuid(), name, quantity, cost);
-        }
+        public decimal Price { get; private set; }
 
-        public static Product Create(Guid id, string name, int quantity, decimal cost)
+        public string Brand { get; private set; }
+
+        public string Model { get; private set; }
+
+        public Transmission Transmission { get; private set; }
+
+        public string MadeIn { get; private set; }
+
+        public int SeatingCapacity { get; private set; }
+
+        public int KmDriven { get; private set; }
+
+        public int Year { get; private set; }
+
+        public FuelType FuelType { get; private set; }
+
+        public string Category { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Description { get; private set; }
+
+        public bool HasInstallment { get; private set; }
+
+        public bool Active { get; private set; }
+
+        public bool Verified { get; private set; }
+
+        public Guid OwnerId { get; private set; }
+
+        public static Product Create(ProductCreateDto dto)
         {
             Product product = new()
             {
-                Id = id,
-                Name = name,
-                Quantity = quantity,
+                Id = Guid.NewGuid(),
+                Name = dto.Name,
+                Price = dto.Price,
+                Brand = dto.Brand,
+                Model = dto.Model,
+                Transmission = (Transmission)Enum.Parse(typeof(Transmission), dto.Transmission),
+                MadeIn = dto.MadeIn,
+                SeatingCapacity = dto.SeatingCapacity,
+                KmDriven = dto.KmDriven,
+                Year = dto.Year,
+                FuelType = (FuelType)Enum.Parse(typeof(FuelType), dto.FuelType),
+                Category = dto.Category,
+                Color = dto.Color,
+                Description = dto.Description,
+                HasInstallment = dto.HasInstallment,
+                OwnerId = dto.OwnerId,
                 Created = DateTime.UtcNow,
-                Active = true,
-                Cost = cost,
+                Active = false,
+                Verified = false,
             };
 
             product.AddDomainEvent(new ProductCreatedIntegrationEvent
             {
                 Id = product.Id,
-                Name = product.Name,
-                Quantity = product.Quantity,
-                ProductCost = product.Cost,
+                OwnerId = product.OwnerId,
             });
 
             return product;
--- CarStore/backend/Product/ProductService.AppCore/UseCases/Commands/CreateProduct.cs
+++ CarStore/backend/Product/ProductService.AppCore/UseCases/Commands/CreateProduct.cs
@@ -8,11 +8,9 @@ namespace ProductService.AppCore.UseCases.Commands
 {
     public class CreateProduct
     {
-        public record Command : ICreateCommand<Command.CreateProductModel, Guid>
+        public record Command : ICreateCommand<ProductCreateDto, Guid>
         {
-            public CreateProductModel Model { get; init; } = default!;
-
-            public record CreateProductModel(string Name, int Quantity, decimal Cost, string ProductCodeName);
+            public ProductCreateDto Model { get; init; } = default!;
 
             internal class Validator : AbstractValidator<Command>
             {
@@ -22,15 +20,23 @@ namespace ProductService.AppCore.UseCases.Commands
                         .NotEmpty().WithMessage("Name is required.")
                         .MaximumLength(50).WithMessage("Name must not exceed 50 characters.");
 
-                    RuleFor(v => v.Model.ProductCodeName)
-                        .NotEmpty().WithMessage("ProductCodeName is required.")
-                        .MaximumLength(5).WithMessage("ProductCodeName must not exceed 5 characters.");
+                    RuleFor(x => x.Model.Price)
+                        .GreaterThan(0).WithMessage("Price should be greater than 0.");
 
-                    RuleFor(x => x.Model.Quantity)
-                        .GreaterThanOrEqualTo(1).WithMessage("Quantity should be at least greater than or equal to 1.");
+                    RuleFor(v => v.Model.Brand)
+                        .NotEmpty().WithMessage("Brand is required.");
 
-                    RuleFor(x => x.Model.Cost)
-                        .GreaterThanOrEqualTo(1000).WithMessage("Cost should be greater than 1000.");
+                    RuleFor(x => x.Model.Model)
+                        .NotEmpty().WithMessage("Model is required.");
+
+                    RuleFor(x => x.Model.Transmission)
+                        .IsEnumName(typeof(Transmission));
+
+                    RuleFor(x => x.Model.FuelType)
+                        .IsEnumName(typeof(FuelType));
+
+                    RuleFor(x => x.Model.OwnerId)
+                        .NotEmpty().WithMessage("OwnerId is required.");
                 }
             }
 
@@ -45,7 +51,8 @@ namespace ProductService.AppCore.UseCases.Commands
 
                 public async Task<ResultModel<Guid>> Handle(Command request, CancellationToken cancellationToken)
                 {
-                    var product = Product.Create(request.Model.Name, request.Model.Quantity,request.Model.Cost);
+                    var product = Product.Create(request.Model);
+
                     var id = await _repository.Add(product);
 
                     return ResultModel<Guid>.Create(id);
