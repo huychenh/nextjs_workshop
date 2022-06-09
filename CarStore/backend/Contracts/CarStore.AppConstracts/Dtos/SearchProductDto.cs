907e57e0fc10904ca954db7f6aeb910a4a2571ab
Quan <quan.hosihong@haravan.com>
2022-06-09 20:29:30 +0700

    add a field in model search

--- CarStore/backend/Contracts/CarStore.AppConstracts/Dtos/SearchProductDto.cs
+++ CarStore/backend/Contracts/CarStore.AppConstracts/Dtos/SearchProductDto.cs
@@ -9,6 +9,7 @@ namespace CarStore.AppContracts.Dtos
     public class SearchProductDto
     {
         public string? SearchText { get; set; }
+        public string? CategoryName { get; set; }
         public decimal PriceFrom { get; set; }
         public decimal PriceTo { get; set; }
         public DateTime? Created { get; set; }
