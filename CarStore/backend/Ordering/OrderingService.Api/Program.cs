e40249173d5de26d5474c69d1e3b08da678f8804
Tung Nguyen Dang <tung.nguyendang@nashtechglobal.com>
2022-06-29 22:19:26 +0700

    Create orders controller

---
+++ CarStore/backend/Ordering/OrderingService.Api/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.IdentityModel.Tokens;
+using N8T.Infrastructure;
+using N8T.Infrastructure.Bus;
+using N8T.Infrastructure.Swagger;
+using N8T.Infrastructure.Validator;
+using OrderingService.Api;
+using OrderingService.AppCore;
+
+const string CorsName = "api";
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+services.AddCors(options =>
+{
+    options.AddPolicy(CorsName, policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+services.AddHttpContextAccessor();
+services.AddCustomMediatR(new[] { typeof(AppCoreAnchor) });
+services.AddCustomValidators(new[] { typeof(AppCoreAnchor) });
+services.AddControllers().AddMessageBroker(builder.Configuration);
+services.AddSwagger(typeof(ApiAnchor));
+// services.AddPostgresDbContext<MainDbContext>(builder.Configuration.GetConnectionString("postgres"));
+// services.AddScoped<IRepository, Repository>();
+
+services.AddAuthentication("token")
+    .AddJwtBearer("token", options =>
+    {
+        options.Authority = builder.Configuration["Auth:Authority"];
+        options.MapInboundClaims = false;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidTypes = new[] { "at+jwt" },
+            NameClaimType = "name",
+            RoleClaimType = "role"
+        };
+    });
+
+var app = builder.Build();
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+var provider = app.Services.GetService<IApiVersionDescriptionProvider>();
+app.UseSwagger(provider);
+app.UseCors(CorsName);
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    // endpoints.MapControllers().RequireAuthorization("ApiCaller");
+    endpoints.MapControllers();
+});
+
+// app.MigrateDatabase();
+app.Run();
---
+++ CarStore/backend/Ordering/OrderingService.Domain/UseCases/Commands/CreateOrder.cs
@@ -0,0 +1,41 @@
+﻿using CarStore.AppContracts.Dtos;
+using FluentValidation;
+using MediatR;
+using N8T.Core.Domain;
+
+namespace OrderingService.AppCore.UseCases.Commands
+{
+    public class CreateOrder
+    {
+        public record Command : ICreateCommand<CreateOrderDto, Guid>
+        {
+            public CreateOrderDto Model { get; init; } = default!;
+
+            internal class Validator : AbstractValidator<Command>
+            {
+                public Validator()
+                {
+                    RuleFor(v => v.Model.ProductId)
+                        .NotEmpty();
+
+                    RuleFor(v => v.Model.Price)
+                        .NotEmpty();
+
+                    RuleFor(v => v.Model.OwnerId)
+                        .NotEmpty();
+
+                    RuleFor(v => v.Model.BuyerId)
+                        .NotEmpty();
+                }
+            }
+
+            internal class Handler : IRequestHandler<Command, ResultModel<Guid>>
+            {
+                public async Task<ResultModel<Guid>> Handle(Command request, CancellationToken cancellationToken)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
