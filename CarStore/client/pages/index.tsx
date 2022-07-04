860cfff82b40557347593ba8ee47238c88e6f5fc
tung.nguyen <tungdn3@gmail.com>
2022-07-04 23:13:17 +0700

    Switch from static generation to client fetching to support team DevOps

--- CarStore/client/pages/index.tsx
+++ CarStore/client/pages/index.tsx
@@ -1,41 +1,67 @@
 import type { NextPage } from 'next';
 import Layout from '../layouts/Layout';
 import CarList from '../components/CarList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ProductService from '../services/ProductService';
 import SearchBox from '../components/SearchBox';
 import BrandSelector from '../components/Brand';
 import BrandService from '../services/BrandService';
+import Pagination from "@material-ui/lab/Pagination";
+import { Models } from '../models/product';
 
-const Home: NextPage = (props: any) => {
-  const { brands } = props;
-  const [cars, setCars] = useState(props.cars || []);
+const Home: NextPage = () => {
+  const [brands, setBrands] = useState([]);
+  const [cars, setCars] = useState<Models.Product[]>([]);
+  const [page, setPage] = useState(1);
+  const [searchText, setSearchText] = useState('');
 
-  const handleSearch = (text: string) => {
-    ProductService.getProducts(text)
+  useEffect(() => {
+    ProductService.getProducts().then(({ data: cars }) => {
+      console.log('cars', cars)
+      setCars(cars);
+    });
+    BrandService.getBrands().then(({ data: brands }) => {
+      setBrands(brands);
+    });
+  }, []);
+
+  useEffect(() => {
+    ProductService.getProducts(searchText, undefined, page)
       .then(response => {
         setCars(response.data);
       });
+  }, [searchText, page]);
+
+  const handleSearchTextChanged = (text: string) => {
+    setSearchText(text);
+    setPage(1);
   }
 
   return (
     <Layout>
-      <SearchBox onSearch={handleSearch} />
+      <SearchBox onSearch={handleSearchTextChanged} />
       <BrandSelector brands={brands} />
+      <Pagination
+        count={cars && cars[0] ? cars[0].totalPages : 1}
+        variant='outlined'
+        color='primary'
+        className='pagination'
+        page={page}
+        onChange={(e: any, value: number) => setPage(value)}
+      />
+      <br />
       <CarList cars={cars} />
+      <Pagination
+        count={cars && cars[0] ? cars[0].totalPages : 1}
+        variant='outlined'
+        color='primary'
+        className='pagination'
+        page={page}
+        onChange={(e: any, value: number) => setPage(value)}
+      />
+      <br />
     </Layout>
   )
-}
-
-export async function getStaticProps() {
-  const { data: cars } = await ProductService.getProducts();
-  const { data: brands } = await BrandService.getBrands();
-  return {
-    props: {
-      cars,
-      brands
-    },
-  }
 }
 
 export default Home
