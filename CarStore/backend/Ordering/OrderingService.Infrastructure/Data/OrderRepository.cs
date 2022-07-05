d430272b092b8590a13f07f62bbd28002c7c0fe8
hai.nguyenphi <phihai.223@gmail.com>
2022-07-05 12:17:39 +0700

    Implement orderService

---
+++ CarStore/backend/Ordering/OrderingService.Infrastructure/Data/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using OrderingService.AppCore;
+using OrderingService.AppCore.Core;
+
+namespace OrderingService.Infrastructure.Data
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly MainDbContext _dbContext;
+
+        public OrderRepository(MainDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Guid> Add(Order order)
+        {
+            if (order == null)
+            {
+                throw new ArgumentNullException(nameof(order));
+            }
+
+            _dbContext.Orders.Add(order);
+
+            await _dbContext.SaveChangesAsync();
+
+            return order.Id;
+        }
+    }
+}
