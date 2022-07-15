91bd46ccc5b2177449a205eac5b442a3f21f01f6
tung.nguyen <tungdn3@gmail.com>
2022-07-15 21:07:22 +0700

    Get buyer and owner emails from BE when creating orders

--- CarStore/backend/Ordering/OrderingService.Domain/UseCases/Commands/CreateOrder.cs
+++ CarStore/backend/Ordering/OrderingService.Domain/UseCases/Commands/CreateOrder.cs
@@ -1,8 +1,10 @@
 ﻿using CarStore.AppContracts.Dtos;
 using FluentValidation;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using N8T.Core.Domain;
 using OrderingService.AppCore.Core;
+using OrderingService.AppCore.Services;
 
 namespace OrderingService.AppCore.UseCases.Commands
 {
@@ -11,8 +13,6 @@ namespace OrderingService.AppCore.UseCases.Commands
         public record Command : ICreateCommand<CreateOrderDto, Guid>
         {
             public CreateOrderDto Model { get; init; } = default!;
-            public string buyerEmail { get; init; } = default!;
-            public string ownerEmail { get; init; } = default!;
 
             internal class Validator : AbstractValidator<Command>
             {
@@ -35,18 +35,42 @@ namespace OrderingService.AppCore.UseCases.Commands
             internal class Handler : IRequestHandler<Command, ResultModel<Guid>>
             {
                 private readonly IOrderRepository _repository;
+                private readonly UserInfoService _userInfoService;
+                private readonly IHttpContextAccessor _httpContextAccessor;
 
-                public Handler(IOrderRepository orderRepository)
+                public Handler(IOrderRepository orderRepository, UserInfoService userInfoService, IHttpContextAccessor httpContextAccessor)
                 {
                     _repository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+                    _userInfoService = userInfoService ?? throw new ArgumentNullException(nameof(userInfoService));
+                    _httpContextAccessor = httpContextAccessor;
                 }
+
                 public async Task<ResultModel<Guid>> Handle(Command request, CancellationToken cancellationToken)
                 {
-                    var order = Order.Create(request.Model, request.buyerEmail, request.ownerEmail);
+                    string ownerEmail;
+                    try
+                    {
+                        ownerEmail = await _userInfoService.GetUserEmail(request.Model.OwnerId);
+                    }
+                    catch
+                    {
+                        // Ignore failure connection
+                        ownerEmail = null;
+                    }
+
+                    var buyerEmail = GetCurrentUserEmail();
+
+                    var order = Order.Create(request.Model, buyerEmail, ownerEmail);
 
                     var id = await _repository.Add(order);
 
                     return ResultModel<Guid>.Create(id);
+                }
+
+                private string GetCurrentUserEmail()
+                {
+                    var emailClaim = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(x => x.Type == "email");
+                    return emailClaim?.Value;
                 }
             }
         }
--- CarStore/backend/Ordering/OrderingService.Infrastructure/Migrations/20220715135337_InitOrderSchema.Designer.cs
+++ CarStore/backend/Ordering/OrderingService.Infrastructure/Migrations/20220715135337_InitOrderSchema.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OrderingService.Infrastructure.Data;
@@ -11,9 +12,10 @@ using OrderingService.Infrastructure.Data;
 namespace OrderingService.Infrastructure.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220715135337_InitOrderSchema")]
+    partial class InitOrderSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
